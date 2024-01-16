97be84b2a2f291e536fc91f27042c34023b58181
YyyyyyH <149113022+YyyyyyH@users.noreply.github.com>
2024-01-16 15:29:21 +0000

    Update images.py

--- src/lfxai/models/images.py
+++ src/lfxai/models/images.py
@@ -37,6 +37,42 @@ def init_vae(img_size, latent_dim, loss_f, name):
 
 
 class EncoderMnist(nn.Module):
+    """
+    This is the q_theta (z|x) network. Given a data sample x q_theta 
+    maps to the latent space x -> z.
+
+    For a VQ VAE, q_theta outputs parameters of a categorical distribution.
+
+    Inputs:
+    - in_dim : the input dimension
+    - h_dim : the hidden layer dimension
+    - res_h_dim : the hidden dimension of the residual block
+    - n_res_layers : number of layers to stack
+
+    """
+
+    def __init__(self, in_dim=1, h_dim=64, n_res_layers=3, res_h_dim=32):
+        super(Encoder, self).__init__()
+        kernel = 4
+        stride = 2
+        self.conv_stack = nn.Sequential(
+            nn.Conv2d(in_dim, h_dim // 2, kernel_size=kernel,
+                      stride=stride, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(h_dim // 2, h_dim, kernel_size=kernel,
+                      stride=stride, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(h_dim, h_dim, kernel_size=kernel-1,
+                      stride=stride-1, padding=1),
+            ResidualStack(
+                h_dim, h_dim, res_h_dim, n_res_layers)
+
+        )
+
+    def forward(self, x):
+        return self.conv_stack(x)
+
+class cEncoderMnist(nn.Module):
     def __init__(self, encoded_space_dim):
         super().__init__()
         self.encoder_cnn = nn.Sequential(
